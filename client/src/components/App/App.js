b1ab8712592c31b01a1af806b06bb31939c0b497
glatvyte <glatvyte@gmail.com>
2022-08-09 21:19:01 +0300

    add api

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import * as finnhub from "finnhub";
 import Filter from "../Filter/Filter";
 import Results from "../Results/Results";
 import "./App.css";
+
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = "cboda52ad3i6ndrm6qo0";
+const finnhubClient = new finnhub.DefaultApi();
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
 
   const search = () => {
-    console.log(inputValue, "inputValue");
     console.log("searchinam");
+    finnhubClient.companyProfile2(
+      { symbol: inputValue },
+      (error, data, response) => {
+        console.log(error, "error");
+        console.log(data, "data");
+        console.log(response, "response");
+      }
+    );
   };
 
   return (
