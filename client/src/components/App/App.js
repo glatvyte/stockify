08940b3ec5d4baa28886c94a58445465135dc5e8
glatvyte <glatvyte@gmail.com>
2022-08-10 08:20:23 +0300

    show api response

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,12 +4,18 @@ import Filter from "../Filter/Filter";
 import Results from "../Results/Results";
 import "./App.css";
 
+//Sukurk Results componenta
+//Sukurti .env
+//sukurti validationa Filtre
+//Uzsaugoti company profile data
+
 const api_key = finnhub.ApiClient.instance.authentications["api_key"];
 api_key.apiKey = "cboda52ad3i6ndrm6qo0";
 const finnhubClient = new finnhub.DefaultApi();
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
+  const [companyList, setCompanyList] = useState([]);
 
   const search = () => {
     console.log("searchinam");
@@ -19,6 +25,13 @@ const App = () => {
         console.log(error, "error");
         console.log(data, "data");
         console.log(response, "response");
+        if (typeof data === Array) {
+          console.log("Arrayjus");
+          setCompanyList(data);
+        } else {
+          console.log("vienas tik");
+          setCompanyList([data]);
+        }
       }
     );
   };
@@ -27,6 +40,7 @@ const App = () => {
     <div className="App">
       <Filter onTermSubmit={search} onInputValueChange={setInputValue} />
       <Results />
+      {companyList[0]?.name}
     </div>
   );
 };
