2e192809a205388f2cabfc3c1ced6c377eec2d7f
glatvyte <glatvyte@gmail.com>
2022-08-11 20:48:41 +0300

    add recharts

--- client/src/components/StockChart/StockChart.js
+++ client/src/components/StockChart/StockChart.js
@@ -1,10 +1,47 @@
-import { useState } from "react";
 import ReactDOM from "react-dom";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
 import "./StockChart.css";
 
 const StockChart = ({ stockCandles, onModalClose, onChangeDates, dates }) => {
+  console.log(dates);
+
+  const transformData = (data) => {
+    return data.c.map((item, index) => ({
+      close: Number(item).toFixed(2),
+      open: Number(data.o[index]).toFixed(2),
+      timestamp: new Date(data.t[index] * 1000).toLocaleDateString(),
+    }));
+  };
+
+  const renderLineChart = () => {
+    return (
+      <LineChart
+        width={800}
+        height={400}
+        data={transformData(stockCandles)}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="timestamp" />
+        <YAxis type="number" allowDecimals={true} allowDataOverflow={true} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="open" stroke="blue" dot={false} />
+        <Line type="monotone" dataKey="close" stroke="gray" dot={false} />
+      </LineChart>
+    );
+  };
+
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active popup-container">
       <div
@@ -22,6 +59,7 @@ const StockChart = ({ stockCandles, onModalClose, onChangeDates, dates }) => {
             clearable={false}
           />
         </div>
+        {renderLineChart()}
         <div className="actions">
           <button onClick={onModalClose} className="ui button negative">
             Close
